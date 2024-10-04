feb3b577e7494839ce241370b994ddba968cb7d0
johnjohn2410 <jross3511@yahoo.com>
2024-10-03 21:50:54 -0500

    Updated file structure and added templates for the expense tracker

--- tracker/views.py
+++ tracker/views.py
@@ -5,7 +5,9 @@ from .forms import ExpenseForm
 # Display all expenses
 def index(request):
     expenses = Expense.objects.all()
+    # Update the path to the template to 'tracker/index.html'
     return render(request, 'tracker/index.html', {'expenses': expenses})
+
 
 # Add a new expense
 def add_expense(request):
@@ -16,4 +18,5 @@ def add_expense(request):
             return redirect('index')
     else:
         form = ExpenseForm()
+    # Update the path to the template to 'tracker/add_expense.html'
     return render(request, 'tracker/add_expense.html', {'form': form})
