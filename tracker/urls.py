c2b4ccb910244e49b99240b08f67dcc5f664569c
johnjohn2410 <jross3511@yahoo.com>
2024-10-03 21:50:54 -0500

    Updated file structure and added templates for the expense tracker

--- tracker/urls.py
+++ tracker/urls.py
@@ -2,13 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('add/', views.add_expense, name='add_expense'),
-]
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('tracker.urls')),  # Include the tracker app's URLs
+    path('', views.index, name='index'),  # Route for the home page
+    path('add/', views.add_expense, name='add_expense'),  # Route for adding expenses
 ]
