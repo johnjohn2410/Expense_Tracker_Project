a9ad58d404d3d275d91c021319187bcf1da1e8cb
johnjohn2410 <jross3511@yahoo.com>
2024-10-06 00:53:25 -0500

    Added user login

--- tracker/views.py
+++ tracker/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render, redirect
 from .models import Expense
 from .forms import ExpenseForm
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
+from .forms import SignUpForm
+def signup(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+        else:
+            form = SignUpForm()
+        return render(request, 'registration/signup.html', {'form': form})
 # Display all expenses
 def index(request):
    category = request.GET.get('category')
