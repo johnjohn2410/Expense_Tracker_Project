92e00d30af09e3499d8999c762e8e5889c83835e
johnjohn2410 <jross3511@yahoo.com>
2024-10-06 19:38:40 -0500

    fixed signup page

--- tracker/urls.py
+++ tracker/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),  # Route for the home page
-    path('add/', views.add_expense, name='add_expense'),# Route for adding expenses
     path('signup/', views.signup, name='signup'),
+    path('', views.index, name='index'),
+    path('add/', views.add_expense, name='add_expense'),
 ]
