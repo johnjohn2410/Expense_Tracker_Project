db2bbe06e0349032093daa9904bff9740caf996d
CHARLESGALPERIN <60405407+CHARLESGALPERIN@users.noreply.github.com>
2024-10-07 23:18:16 -0500

    changed 'home' to 'index' @ line38 in views, added clickable date in the add expense form, edited & added categories in the expense model.

--- tracker/views.py
+++ tracker/views.py
@@ -35,7 +35,7 @@ def user_login(request):
             user = authenticate(request, username=username, password=password)
             if user:
                 login(request, user)
-                return redirect('home')
+                return redirect('index')
     else:
         form = LoginForm()
     return render(request, 'registration/login.html', {'form': form})
